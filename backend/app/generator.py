226b9b836824a7ddf99449a8da24c5b7122dfb03
shepelenvlad <156517213+shepelenvlad@users.noreply.github.com>
2026-09-18 15:49:47 +0200

    fix

--- backend/app/generator.py
+++ backend/app/generator.py
@@ -67,7 +67,7 @@ _FIELD_NAME_GENERATORS: list[tuple[re.Pattern, Any]] = [
 _FORMAT_GENERATORS = {
     "email": lambda: fake.email(),
     "date": lambda: fake.date(),
-    "date-time": lambda: fake.iso8601(),
+    "date-time": lambda: fake.date(pattern='%m/%d/%Y'),
     "uuid": lambda: str(fake.uuid4()),
     "uri": lambda: fake.url(),
     "url": lambda: fake.url(),
